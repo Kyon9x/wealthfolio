4317d5678f92378165615e52e66f4e274f22d0d0
chipheo <chipheo@cfpm.com>
2025-10-30 17:59:46 +0700

    [fix] get rid off vn_market_service

--- services/vn-market-service/app/clients/__init__.py
+++
@@ -1,3 +0,0 @@
-from .fund_client import FundClient
-
-__all__ = ["FundClient"]
--- services/vn-market-service/app/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-PORT = int(os.getenv("VN_MARKET_SERVICE_PORT", "8765"))
-HOST = os.getenv("VN_MARKET_SERVICE_HOST", "127.0.0.1")
-CORS_ORIGINS = ["tauri://localhost", "http://localhost:1420"]
